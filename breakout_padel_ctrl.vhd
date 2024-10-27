9d18c64c19936d3b5128725aeb07aca23edfa0af
Ludvig <ludvig.snihs@outlook.com>
2024-10-27 16:28:05 +0100

    Initial issue

---
+++ breakout_padel_ctrl.vhd
@@ -0,0 +1,89 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+--library work;
+use work.breakout_pkg.all;
+
+entity breakout_paddle_ctrl is
+    generic (
+        g_PLAYER_paddle_Y : integer
+    );
+    port (
+        i_CLK           : in std_logic;
+        i_LEFT          : in std_logic;
+        i_RIGHT         : in std_logic;
+        i_col_count_div : in std_logic_vector(6 downto 0);
+        i_row_count_div : in std_logic_vector(6 downto 0);
+        o_draw_paddle    : out std_logic;
+        o_paddle_X       : out std_logic_vector(6 downto 0)
+    );
+end entity;
+
+architecture rtl of breakout_paddle_ctrl is
+
+    signal w_col_index : natural range 0 to (2 ** i_col_count_div'length) := 0;
+    signal w_row_index : natural range 0 to (2 ** i_row_count_div'length) := 0;
+
+    signal r_paddle_count : natural range 0 to c_paddle_SPEED := 0;
+
+    -- Start location of paddle
+    signal r_paddle_X : natural range 0 to (c_GAME_WIDTH_END - c_paddle_WIDTH - 1) := (c_GAME_WIDTH_END + c_GAME_HEIGHT_START - c_paddle_WIDTH - 1)/2; -- probably shouldnt start at 0
+
+    signal r_draw_paddle : std_logic := '0';
+begin
+    w_col_index <= to_integer(unsigned(i_col_count_div));
+    w_row_index <= to_integer(unsigned(i_row_count_div));
+
+    p_paddle_counter : process (i_CLK)
+    begin
+        if rising_edge(i_CLK) then
+            if (i_LEFT = '1') xor (i_RIGHT = '1') then
+                if (r_paddle_count = c_paddle_SPEED) then
+                    r_paddle_count <= 0;
+                else
+                    r_paddle_count <= r_paddle_count + 1;
+                end if;
+            else
+                r_paddle_count <= 0;
+            end if;
+        end if;
+    end process;
+
+    p_move_paddle : process (i_CLK)
+    begin
+        if rising_edge(i_CLK) then
+            if (i_LEFT = '1') and (r_paddle_count = c_paddle_SPEED) then
+                if (r_paddle_X = 0) then
+                    r_paddle_X <= 0;
+                else
+                    r_paddle_X <= r_paddle_X - 1;
+                end if;
+            elsif (i_RIGHT = '1') and (r_paddle_count = c_paddle_SPEED) then
+                if (r_paddle_X = (c_GAME_WIDTH - c_paddle_WIDTH - 1)) then
+                    r_paddle_X <= (c_GAME_WIDTH - c_paddle_WIDTH - 1);
+                else
+                    r_paddle_X <= r_paddle_X + 1;
+                end if;
+            else
+                r_paddle_X <= r_paddle_X;
+            end if;
+        end if;
+    end process;
+
+    p_draw_paddle : process (i_CLK)
+    begin
+        if rising_edge(i_CLK) then
+            if (w_row_index = g_PLAYER_paddle_Y) and
+                (w_col_index >= r_paddle_X) and
+                (w_col_index <= r_paddle_X + c_paddle_WIDTH) then
+                r_draw_paddle <= '1';
+            else
+                r_draw_paddle <= '0';
+            end if;
+        end if;
+    end process;
+    o_draw_paddle <= r_draw_paddle;
+    o_paddle_X    <= std_logic_vector(to_unsigned(r_paddle_X, o_paddle_X'length));
+
+end architecture;
---
+++ breakout_pkg.vhd
@@ -0,0 +1,21 @@
+package breakout_pkg is
+
+    -- Tile scaled by 16: 640-480 ==> 40-30
+    --constant c_GAME_WIDTH      : integer := 80;
+    --constant c_GAME_HEIGHT     : integer := 60;
+    --constant c_GAME_CEIL       : integer := 5;
+    constant c_PLAYER_PADDLE_Y   : integer := c_GAME_HEIGHT_END - 1;
+    constant c_PADDLE_SPEED      : integer := 250_000; -- 1 tile movement every 10 ms 
+    constant c_PADDLE_HEIGHT     : integer := 1;
+    constant c_GAME_WIDTH_START  : integer := 4;
+    constant c_GAME_WIDTH_END    : integer := 75;
+    constant c_GAME_HEIGHT_START : integer := 5;
+    constant c_GAME_HEIGHT_END   : integer := 60;
+
+    constant c_PADDLE_WIDTH : integer := 8;
+    constant c_BALL_SPEED   : integer := 100_000;--1_000_000;
+
+    constant c_SCORE_LIMIT : integer := 9;
+    -- If work.entity gives you problems, declare components
+    -- in here and just instantiate in architecture.
+end package;
